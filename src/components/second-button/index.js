6f1624b0a3433f3ed29cfc3e3062c28e3fee58d3
Bruno  Cota <brunocota@MacBook-Air-de-Bruno.local>
2024-06-13 11:05:09 -0300

    fix/end

--- src/components/second-button/index.js
+++ src/components/second-button/index.js
@@ -2,6 +2,6 @@ import { Link } from "react-router-dom";
 
 export const SecondButton = ({ url }) => {
     return (
-        <Link to={url} className="btn-third lusitana-regular text-center">REQUEST A QUOTE</Link>
+        <Link to={url} className="btn-third lusitana-regular text-center">REQUEST QUOTE</Link>
     );
 } 
